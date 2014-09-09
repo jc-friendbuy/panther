184430efbe98e64e4272f0e38dbc83d85016038f
Juan Carlos Coto <jccotou@gmail.com>
2014-09-09 16:34:11 -0600

    Before rename

--- app/server.R
+++ app/server.R
@@ -1,27 +1,11 @@
 library(shiny)
-library(modules)
 source('src/analysis/basic.R')
 
-# shinyServer(router$Route2)
 shinyServer(function(input, output) {
-    
-    # Expression that generates a histogram. The expression is
-    # wrapped in a call to renderPlot to indicate that:
-    #
-    #  1) It is "reactive" and therefore should re-execute automatically
-    #     when inputs change
-    #  2) Its output type is a plot
-    
+    # Determine the appropriate render function for the selected visualization, and     
+    # query / calculate the results.  Then, return it so it can be displayed on the UI.
     output$visualization <- renderPlot({
-        
         data.to.plot <- SelectSimpleDrugSubset()
-#         plot(data.to.plot$x, data.to.plot$y)
         do.call(data.to.plot$fun, data.to.plot$args)
-        
-#         x    <- faithful[, 2]  # Old Faithful Geyser data
-#         bins <- seq(min(x), max(x), length.out = input$bins + 1)
-#         
-#         # draw the histogram with the specified number of bins
-#         hist(x, breaks = bins, col = 'darkgray', border = 'white')
     })
 })
