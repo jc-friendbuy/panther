37e15080e60c8a74405686ca13411573a30ec27e
Juan Carlos Coto <jccotou@gmail.com>
2014-09-19 01:07:32 -0600

    Continue hacking persp3d with data.table

--- hack_persp_drug.R
+++ hack_persp_drug.R
@@ -1,9 +1,12 @@
+library(data.table)
 the.data <- read.csv('data/drug.csv')
 the.data <- data.table(the.data)
-x <- the.data$CCLE.Cell.Line.Name
-y <- the.data$Compound
-z <- the.data$IC50..uM.
-z[is.na(z)] <- -1
 
+setkey(the.data, Compound)
+
+
+# x is the x coordinate vector
+# y is the y coordinate vector
+# z is a dim(x) x dim(y) matrix with the data that will be plotted on the persp
 open3d()
 persp3d(x, y, z)
