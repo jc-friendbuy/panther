57d037249d7f4e55458f21af8becbda91aa9a711
Juan Carlos Coto <jccotou@gmail.com>
2014-09-25 11:50:14 -0600

    Modified reader functions to correctly parse data files

--- setup/database/etl/common/data_readers.py
+++ setup/database/etl/common/data_readers.py
@@ -6,20 +6,23 @@ class CCLEDataReader(object):
     def __init__(self, data_home_path='~/Projects/panther/data/'):
         self._data_home_dir = data_home_path
 
-    def read(file_name):
+    def read(self, file_name):
         computed_file_path = self._get_file_path(file_name)
         return self._read(computed_file_path)
 
-    def _get_file_path(file_name):
+    def _get_file_path(self, file_name):
         full_path = os.path.join(self._data_home_dir, file_name)
-        return os.expanduser(full_path).normcase()
+        expanded_path = os.path.expanduser(full_path)
+        return os.path.normcase(expanded_path)
 
 
 
 class CopyNumberReader(CCLEDataReader):
 
     def _read(self, computed_file_path):
-        return pandas.read_csv(computed_file_path, index_col=0)
+        return pandas.read_csv(computed_file_path,
+                               index_col=0,
+                               delim_whitespace=True)
 
 class DrugResponseReader(CCLEDataReader):
 
@@ -29,9 +32,14 @@ class DrugResponseReader(CCLEDataReader):
 class GeneExpressionReader(CCLEDataReader):
 
     def _read(self, computed_file_path):
-        return pandas.read_csv(computed_file_path, skiprows=2, index_col=0)
+        return pandas.read_csv(computed_file_path,
+                               skiprows=2,
+                               index_col=0,
+                               delim_whitespace=True)
 
 class CellLineInformationReader(CCLEDataReader):
 
     def _read(self, computed_file_path):
-        return pandas.read_csv(computed_file_path, index_col=0)
+        return pandas.read_csv(computed_file_path,
+                               index_col=0,
+                               quotechar='"')
---
+++ setup/database_setup.py
@@ -0,0 +1,7 @@
+import ipdb
+from setup.database.etl import extract, transform, load
+
+
+if __name__ == '__main__':
+    ipdb.set_trace()
+    extract.load()
