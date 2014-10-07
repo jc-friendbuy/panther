4e056f8ec81f53ecd08f82d1176c5d70c2f11c29
Juan Carlos Coto <jccotou@gmail.com>
2014-10-06 22:45:02 -0600

    Add project home to main file

--- setup/database_setup.py
+++ setup/database_setup.py
@@ -1,4 +1,9 @@
+import sys
+from os import path
+sys.path.append(path.expanduser('~/Projects/panther/'))
+
 from setup.database.etl.processors import main
 
 if __name__ == '__main__':
+
     main.CancerCellLineEncyclopediaETLETLProcessor().load()
