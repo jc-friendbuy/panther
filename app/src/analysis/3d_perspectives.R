dfe75d620e86c8b24d6e1e9016279a6dda45b2fb
Juan Carlos Coto <jccotou@gmail.com>
2014-10-15 10:23:48 -0600

    Add function to get the expression and copy number data

---
+++ app/src/analysis/3d_perspectives.R
@@ -0,0 +1,102 @@
+source('src/data/db.R')
+library(shinyRGL)
+
+GetExpressionAndCopyNumber <- function() {
+  ApplyByGene(function(data, gene.label) {
+    print(paste("Computing for", gene.label))
+    data$quantileNormalizedRMAExpression, data$snpCopyNumber2Log2)
+  })
+}
+
+ExpressionAndCopyNumberPerspectives <- function() {
+  
+}
+
+CorrelationHistogram <- function(data) {
+  expression.and.copy.number <- GetExpressionAndCopyNumber()
+  list(
+    list(
+      graph.type = "plot3d",
+      visualization = function() {
+        
+        lights3d()
+        
+        
+        
+        hist(x = unlist(correlations.by.gene),
+             xlab = "Correlation",
+             ylab = "Frequency",
+             main = "Copy Number, Expression Correlation")
+      }
+    )
+  )
+
+# Drug data - not ready
+# drug.data <- data.table(read.csv('data/drug.csv'))
+# # Assign 100 as the IC50 of NA values (an abnormally high number)
+# drug.data[is.na(IC50..uM.),IC50..uM. := 100]
+
+
+# GENE EXPRESSION
+
+
+
+# Expression data - read only from 500 genes
+kNumGenes <- 500
+expression.data <- read.table('data/expression.gct',
+                              header = T,
+                              skip = 2,
+                              sep = '\t',
+                              nrows = kNumGenes)
+
+
+x <- 1:kNumGenes
+# y <- 1:(dim(expression.data)[2] - 2)
+# z <- data.matrix(expression.data[1:kNumGenes, 3:dim(expression.data)[2]])
+# Select only 60 cell lines (for demo purposes only)
+y <- 1:60
+# Remove the first two columns (name and description) from the data to graph it
+z <- data.matrix(expression.data[1:kNumGenes, 3:62])
+
+
+# x is the x coordinate vector
+# y is the y coordinate vector
+# z is a dim(x) x dim(y) matrix with the data that will be plotted on the persp
+
+# Interactive 3d perspective
+open3d()
+persp3d(x, y, z, xlab = "Gen", ylab = "Linea", zlab = "Nivel expresion",
+        col = 'lightblue')
+
+# Pre-rendered and non-dynamic 3d perspective.
+persp(x, y, z, xlab = "Gen", ylab = "Linea", zlab = "Nivel expresion",
+      col = 'lightblue', theta = 15, phi = 45)
+
+
+
+# COPY NUMBER
+
+
+# Copy number data - read only from kCopyNumGenes genes
+kCopyNumGenes <- 1500
+copy.number.data <- read.table('data/copynumber.txt',
+                               header = T,
+                               sep = '\t',
+                               nrows = kCopyNumGenes)
+
+x <- 1:kCopyNumGenes
+y <- 1:60
+z <- data.matrix(copy.number.data[1:kCopyNumGenes, 6:65])
+# x is the x coordinate vector
+# y is the y coordinate vector
+# z is a dim(x) x dim(y) matrix with the data that will be plotted on the persp
+
+# Interactive 3d perspective
+open3d()
+persp3d(x, y, z, xlab = "Gen", ylab = "Linea", zlab = "Copy Number",
+        col = 'lightblue')
+
+# Pre-rendered, non dynamic 3d perspective.
+persp(x, y, z, xlab = "Gen", ylab = "Linea", zlab = "Copy Number",
+      col = 'lightblue', theta = 15, phi = 45)
+
