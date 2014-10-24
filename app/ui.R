1f9db9a7bcc711631a6ddb960a019570dc2e1ee2
Juan Carlos Coto <jccotou@gmail.com>
2014-10-24 10:46:26 -0600

    Erase unnnecesarry comments on ui file

--- app/ui.R
+++ app/ui.R
@@ -2,29 +2,6 @@ library(shiny)
 library(shinyRGL)
 source('visualization_config.R')
 
-# Define UI for application that draws a histogram
-# shinyUI(fluidPage(
-# 
-#     titlePanel("Prototipo de visualización de datos"),
-# 
-#     sidebarLayout(
-#         sidebarPanel(
-#             selectInput("visualization.select",
-#                         label = h3("Visualización"),
-#                         choices = GetVisualizationFunctionChoices(),
-#                         selected = 2),
-#             actionButton("btn.display.selected.visualization",
-#                          label = "Mostrar")
-#         ),
-# 
-#         mainPanel(
-#             plotOutput("visualization"),
-#             webGLOutput("vis3d")
-#         ),
-#         position = "right"
-#     )
-# ))
-
 shinyUI(fluidPage(
   
   titlePanel("Prototipo de visualización de datos"),
